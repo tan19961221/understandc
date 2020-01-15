5af3f1435962c952d73c53ad7b8ed4188baa2e68
tan19961221 <edz@tandongdong.local>
2020-01-15 10:14:53 +0800

    ccc

---
+++ 4-18.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void) 
+{
+    int a;
+    printf("请输入整数\n");
+    scanf("%d", &a);
+    int b = 0;
+
+    for (int i = 0; i < a; i++)
+     {
+        if (b == 5) 
+        {
+            printf("\n");
+            b = 0;
+        }
+
+        printf("*");
+        b = b + 1;
+    }
+}
---
+++ 4-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void) {
+    int n1, n2, sum, x, min, max;
+    sum = 0;
+    scanf("%d %d", &n1, &n2);
+
+    if (n1 > n2) {
+        x = n1 - n2;
+        max = n1;
+        min = n2;
+    } else {
+        x = n2 - n1;
+        max = n2;
+        min = n1;
+    }
+
+    do {
+        x = x - 1;
+        sum += min + x;
+    } while (x != 0);
+
+    sum += max;
+    printf("大于等于%d 小于等于%d 的所有整数的和是%d", min, max, sum);
+    return 0;
+}
